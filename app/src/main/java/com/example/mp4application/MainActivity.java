16adfef51a2564affc9c393499ffee5f07814838
weishuyong1996 <svipwsy@163.com>
2019-05-01 09:32:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mp4application/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mp4application;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(getApplicationContext(),ActivityWebView.class));
+    }
+}
---
+++ app/src/main/java/com/example/mp4application/VideoConstant.java
@@ -0,0 +1,22 @@
+package com.example.mp4application;
+
+/**
+ * Created by shengjun on 16/9/10.
+ */
+public class VideoConstant {
+
+    public static String[] videoUrlList =
+            {
+                    "http://jzvd.nathen.cn/c6e3dc12a1154626b3476d9bf3bd7266/6b56c5f0dc31428083757a45764763b0-5287d2089db37e62345123a1be272f8b.mp4"
+            };
+
+    public static String[] videoThumbList =
+            {
+
+                    "http://jzvd-pic.nathen.cn/jzvd-pic/f03cee95-9b78-4dd5-986f-d162c06c385c.png",
+
+
+            };
+
+
+}
